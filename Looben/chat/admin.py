fc76efdc17a7f69b72f8a6e8a30453e83432f885
KantaNakamura <0706363@gmail.com>
2022-10-21 09:45:14 +0800

    register chat.models.Messages to admin

--- Looben/chat/admin.py
+++ Looben/chat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Messages
 
-# Register your models here.
+
+class MessagesAdmin(admin.ModelAdmin):
+    list_display=('pk','description','sender_name', 'receiver_name','time', 'seen', 'timestamp')
+
+
+admin.site.register(Messages, MessagesAdmin)
