2c17a350bd7f31ded80f3e4c607aa2773edaeb7f
KantaNakamura <0706363@gmail.com>
2022-10-21 08:56:23 +0800

    create chat_room.html

--- Looben/chat/views.py
+++ Looben/chat/views.py
@@ -9,7 +9,7 @@ from chat.models import Messages
 def getFriendsList(username):
     try:
         user = Users.objects.get(username=username)
-        friends = list(user.connection_users.all())
+        friends = list(user.connection.all())
         return friends
     except:
         return []
@@ -23,17 +23,24 @@ def get_message(request, username):
     current_user = Users.objects.get(username=request.user.username)
     messages = Messages.objects.filter(sender_name=current_user.id, receiver_name=friend.id) | \
                Messages.objects.filter(sender_name=friend.id, receiver_name=current_user.id)
-    friends = getFriendsList(request.user.username)
+    friends_list = getFriendsList(request.user.username)
     return render(request, "chat/messages.html", {
         'messages': messages,
-        'friends': friends,
+        'friends_list': friends_list,
         'current_user': current_user, 
         'friend': friend
         })
     
     
 class ChatRoomView(DetailView):
-    template_name = 'chat/messages.html' 
-    models = Users
+    template_name = 'chat/chat_room.html' 
+    model = Users
     slug_field = 'username'
     slug_url_kwarg = 'username'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.object
+        context['friends_list'] = getFriendsList(user.username)
+        context['amount_of_friends'] = user.connection.all().count()
+        return context
