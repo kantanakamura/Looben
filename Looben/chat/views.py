ad2e47373ae2f005a041fb2fa1176513113e2bea
KantaNakamura <0706363@gmail.com>
2022-11-13 17:32:16 +0800

    add messages seen effects

--- Looben/chat/views.py
+++ Looben/chat/views.py
@@ -7,17 +7,8 @@ from rest_framework.parsers import JSONParser
 from chat.serializers import MessageSerializer
 from django.views.generic.base import View
 
-from accounts.models import Users
+from accounts.models import Users, FollowForUser
 from chat.models import Messages
-
-
-def getFriendsList(username):
-    try:
-        user = Users.objects.get(username=username)
-        friends = list(user.connection.all())
-        return friends
-    except:
-        return []
     
     
 def get_message(request, username):
@@ -28,18 +19,18 @@ def get_message(request, username):
     current_user = Users.objects.get(username=request.user.username)
     messages = Messages.objects.filter(sender_name=current_user.id, receiver_name=conversation_partner.id) | \
                Messages.objects.filter(sender_name=conversation_partner.id, receiver_name=current_user.id)
-    amount_of_friends = request.user.connection.all().count()
-    friends_list = getFriendsList(request.user.username)
+    amount_of_following_users = FollowForUser.objects.filter(user=request.user).count()
+    following_user_list =  FollowForUser.objects.filter(user=request.user)
     for message in messages:
         if message.sender_name == conversation_partner:
             message.seen = True
             message.save()
     return render(request, "chat/messages.html", {
         'messages': messages,
-        'friends_list': friends_list,
+        'following_user_list': following_user_list,
         'current_user': current_user, 
         'conversation_partner': conversation_partner,
-        'amount_of_friends': amount_of_friends,
+        'amount_of_following_users': amount_of_following_users,
         })
     
     
@@ -52,8 +43,8 @@ class ChatRoomView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.object
-        context['friends_list'] = getFriendsList(user.username)
-        context['amount_of_friends'] = user.connection.all().count()
+        context['following_user_list'] = FollowForUser.objects.filter(user=user)
+        context['amount_of_following_users'] = FollowForUser.objects.filter(user=user).count()
         return context
         
         
