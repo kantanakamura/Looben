3d890dd4ad27fa8058b6ba2fb6e515953b6c91f3
KantaNakamura <0706363@gmail.com>
2022-11-15 16:53:51 +0800

    change sidebar of review and question view

--- Looben/reviews/urls.py
+++ Looben/reviews/urls.py
@@ -5,5 +5,5 @@ app_name = 'reviews'
 
 urlpatterns = [
     path('create_review/', views.create_review_of_university, name='create_review_of_university'),
-    path('review_list/<int:pk>', views.ReviewListOfUniversities.as_view(), name='review_list_of_universities'),
+    path('review_list_of_universities/<int:pk>', views.ReviewListOfUniversities.as_view(), name='review_list_of_universities'),
 ]
