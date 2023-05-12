186a2d4dc62ce0b99fdb1527e0501a25f76c31df
KantaNakamura <0706363@gmail.com>
2023-05-12 14:04:10 +0800

    add test db

--- Looben/settings.py
+++ Looben/settings.py
@@ -101,6 +101,14 @@ DATABASES = {
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': os.environ.get('DATABASE_PORT')
+    },
+    'test': {
+        'ENGINE': os.environ.get('DATABASE_ENGINE'),
+        'NAME': os.environ.get('DATABASE_NAME_TEST'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT')
     }
 }
 
