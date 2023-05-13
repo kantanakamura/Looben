17af0f75beee5f32864f548dcb4279b12efd677b
KantaNakamura <0706363@gmail.com>
2023-05-13 22:37:06 +0800

    refactor settings

--- Looben/settings.py
+++ Looben/settings.py
@@ -102,14 +102,6 @@ DATABASES = {
         'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': os.environ.get('DATABASE_PORT')
     },
-    'test': {
-        'ENGINE': os.environ.get('DATABASE_ENGINE'),
-        'NAME': os.environ.get('DATABASE_NAME_TEST'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT')
-    }
 }
 
 
