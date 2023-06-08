ad7d0d6a148ba63b47d48d78615dede1d7c0f615
KantaNakamura <0706363@gmail.com>
2023-06-08 19:21:00 +0800

    refactor setting

--- Looben/settings.py
+++ Looben/settings.py
@@ -1,7 +1,6 @@
 import environ
 import os
 from pathlib import Path
-from telnetlib import AUTHENTICATION
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
