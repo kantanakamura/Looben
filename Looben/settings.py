5a07218506ab9eff140f84becff42a25aaf4cd57
KantaNakamura <0706363@gmail.com>
2023-06-08 19:51:25 +0800

    refacotor test db

--- Looben/settings.py
+++ Looben/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
 DATABASES['test'] = {
     'default': {
         'ENGINE': os.environ.get('DATABASE_ENGINE'),
-        'NAME': os.environ.get('DATABASE_NAME'),
+        'NAME': os.environ.get('DATABASE_NAME_TEST'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
