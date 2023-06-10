c4385ba1d3a4a69c58b472ae524a0018065c01ef
KantaNakamura <0706363@gmail.com>
2023-06-10 13:26:02 +0800

    rafactor settings.py

--- Looben/settings.py
+++ Looben/settings.py
@@ -106,16 +106,16 @@ DATABASES = {
     }
 }
 
-# DATABASES['test'] = {
-#     'default': {
-#         'ENGINE': os.environ.get('DATABASE_ENGINE'),
-#         'NAME': os.environ.get('DATABASE_NAME_TEST'),
-#         'USER': os.environ.get('DATABASE_USER'),
-#         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-#         'HOST': os.environ.get('DATABASE_HOST'),
-#         'PORT': os.environ.get('DATABASE_PORT')
-#     }
-# }
+DATABASES['test'] = {
+    'default': {
+        'ENGINE': os.environ.get('DATABASE_ENGINE'),
+        'NAME': os.environ.get('DATABASE_NAME_TEST'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT')
+    }
+}
 
 
 
